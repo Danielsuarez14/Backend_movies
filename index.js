5e4e5ff2636f1e4d0d891775fa46c9c5a8d99a3a
Daniel Suarez <danielsuarez2418@gmail.com>
2025-03-26 09:15:07 -0500

    Delete dirname

--- index.js
+++ index.js
@@ -3,18 +3,14 @@ import cors from 'cors'
 import { PORT } from './config.js'
 import booksRoutes from './routes/routes_book.js'
 import userRoutes from './routes/routes_user.js'
-import {dirname, join} from 'path'
-import { fileURLToPath } from 'url'
 import passport from 'passport'
+import './utils/auth/index.js'
 const app = express()
-const __dirname = dirname(fileURLToPath(import.meta.url))
 app.use(cors())
 app.use(passport.initialize())
-import './utils/auth/index.js'
 app.use(express.json())
 app.use(userRoutes)
 app.use(booksRoutes)
-app.use(express.static(join(__dirname, '../client/dist')))
 app.listen(PORT, ()=>{
     console.log(`Server running in the port: ${PORT}`)
 })
