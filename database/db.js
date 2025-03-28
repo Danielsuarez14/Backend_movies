3a6b969141d19911f3813c1caec4a98de86d0bf5
Daniel Suarez <danielsuarez2418@gmail.com>
2025-03-28 12:27:30 -0500

    Fix

--- database/db.js
+++ database/db.js
@@ -1,4 +1,4 @@
-import { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME} from "../config.js";
+import { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT} from "../config.js";
 import { createPool } from "mysql2/promise";
 export const pool = createPool({
     host: DB_HOST,
